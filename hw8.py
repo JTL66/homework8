6dfce88698b28c39714c76793589d620dc253c99
JTL66 <65905010+JTL66@users.noreply.github.com>
2020-10-30 22:51:21 -0400

    Add files via upload

--- hw8.py
+++ hw8.py
@@ -1,41 +1,11 @@
 
 ##1.
 
-class Animal:
-    hints = {
-        'elephant':['I have exceptional memory','I am the largest land-living mammal in the world','I have big ears'],
-        'tiger':['I am the biggest cat','I come in black and white or orange and black','I am the king of the forest'],
-        'bat':['I use echo-location', 'I can fly', 'I see well in dark']
-    }
-    def __init__(self, name):
-        self.name = name
-        self.index_hint = 0
-
-    def guess_who_am_i(self):
-        print('I will give you 3 hints, guess what animal I am\n')
-        while self.index_hint < 3:
-            print(self.hints[self.name][self.index_hint])
-            self.index_hint += 1
-            guess = input("Who am I?:" )
-            if guess == self.name:
-                print("You got it! I am {}\n".format(self.name))
-                return
-            print('Nope, try again!\n')
-
-        print("I'm out of hints! The answer is: {}\n".format(self.name))
-
-
-e = Animal("elephant")
-t = Animal("tiger")
-b = Animal("bat")
-e.guess_who_am_i()
-t.guess_who_am_i()
-b.guess_who_am_i()
 
 
 
 
-##2. 
+##2.
 
 from random import randrange
 a = randrange(5)
